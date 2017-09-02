67f9d9430572cebda9ac686dd3e53ede7a7007de
MJingv <625619954@qq.com>
2017-09-02 21:20:49 +0800

    finish all the funciton and effects about singer-page

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -14,13 +14,43 @@ export function hasClass(el, className) {
   return reg.test(el.className)
 }
 
-export function getData(el,name,val){
+export function getData(el, name, val) {
   //获取属性的具体值
   const prefix = 'data-'
-  name = prefix +name
-  if(val){
-    return el.setAttribute(name ,val)
-  }else{
+  name = prefix + name
+  if (val) {
+    return el.setAttribute(name, val)
+  } else {
     return el.getAttribute(name)
   }
+}
+
+let elementStyle = document.createElement('div').style
+
+let vendor = (() => {
+  //检测供应商
+  let transformNames = {
+    webkit: 'webkitTransform',
+    Moz: 'MozTransform',
+    O: 'OTransform',
+    ms: 'msTransform',
+    standard: 'transform'
+  }
+  for (let key in transformNames) {
+    if (elementStyle[transformNames[key]] !== undefined) {
+      return key
+    }
+  }
+  return false
+})()
+
+export function prefixStyle(style) {
+  if (vendor === false) {
+    return false
+  }
+  if (vendor === 'standard') {
+    return style
+  }
+  //拼接字符串，style：供应商+style首字母大写+剩余style
+  return vendor + style.charAt(0).toUpperCase()+style.substring(1)
 }
--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,21 +1,28 @@
 <template>
 <div class="music-list">
-  <div class="back">
+  <div class="back" @click="back">
     <i class="icon-back"></i>
   </div>
   <h1 class="title" v-html="title"></h1>
   <!-- title会有转义字符，推荐html -->
 
   <div class="bg-image" :style="bgStyle" ref='bgImage'>
+    <div class="play-wrapper">
+      <div class="play" v-show="songs.length>0" ref ="playBtn">
+        <i class="icon-play"></i>
+        <span class="text">随机播放全部</span>
+      </div>
+    </div>
     <div class="filter" ref="filter">
     </div>
   </div>
-
-  <scroll :data="songs" class="list" ref='list'>
+  <div class="bg-layer" ref="layer"> </div>
+  <scroll @scroll="scroll" :data="songs" class="list" ref='list' :probe-type="probeType" :listenScroll="listenScroll">
     <div class="song-list-wrapper">
       <song-list :songs="songs"></song-list>
     </div>
   </scroll>
+
 
 </div>
 </template>
@@ -23,6 +30,16 @@
 <script>
 import Scroll from 'base/scroll/scroll'
 import SongList from 'base/song-list/song-list'
+import {
+  prefixStyle
+} from 'common/js/dom'
+
+const RESERVED_HEIGHT = 40
+const transform = prefixStyle('transform')
+const backdrop = prefixStyle('backdrop-filter')
+
+
+
 export default {
   props: {
     bgImage: {
@@ -38,17 +55,77 @@ export default {
       default: ''
     }
   },
+  created() {
+    this.probeType = 3
+    this.listenScroll = true
+  },
+  data() {
+    return {
+      scrollY: 0,
+
+    }
+  },
+  methods: {
+    scroll(pos) {
+      //实时拿到y的值
+      this.scrollY = pos.y
+    },
+    back() {
+      this.$router.back()
+    }
+  },
+  watch: {
+    scrollY(newY) {
+      //滚动layer不超过bg-image高度
+      let translateY = Math.max(this.minTranslateY, newY)
+      let zIndex = 0
+      let scale = 1
+      let blur = 0
+      this.$refs.layer.style[transform] = `translate3d(0,${translateY}px,0)`
+
+      const percent = Math.abs(newY / this.imageHeight)
+
+      //z-index实现跟随放大缩小隐藏等效果
+      if (newY > 0) {
+        //向下拉list实现放大效果
+        zIndex = 10
+        scale = 1 + percent
+      } else {
+        //list超过bg-img时实现动态高斯模糊效果
+        blur = Math.min(20 * percent, 20)
+      }
+      this.$refs.filter.style[backdrop] = `blur(${blur}px)`
+
+      console.log(this.$refs.filter.style[backdrop] + 'blur effect ')
+
+
+      if (newY < this.minTranslateY) {
+        //滚到了title处
+        zIndex = 10
+        this.$refs.bgImage.style.paddingTop = 0
+        this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
+        this.$refs.playBtn.style.display='none'
+      } else {
+        this.$refs.bgImage.style.paddingTop = '70%'
+        this.$refs.bgImage.style.height = 0
+
+        this.$refs.playBtn.style.display=''
+      }
+      this.$refs.bgImage.style.zIndex = zIndex
+      this.$refs.bgImage.style[transform] = `scale(${scale})`
+    }
+  },
   computed: {
     bgStyle() {
-      console.log(this.bgImage);
       return `background-image:url(${this.bgImage})`
     },
 
   },
   mounted() {
+    this.imageHeight = this.$refs.bgImage.clientHeight
+    this.minTranslateY = -this.imageHeight + RESERVED_HEIGHT
+    this.$refs.list.$el.style.top = `${this.imageHeight}px`
 
-    this.$refs.list.$el.style.top = `${this.$refs.bgImage.clientHeight}px`
-    console.log(this.$refs.list.$el.style.top);
   },
   components: {
     SongList,
