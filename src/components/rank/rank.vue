0ae7fe59872e2ee8e411c0a764d6bf31dc421618
MJingv <625619954@qq.com>
2018-04-21 13:49:02 +0800

    update rank

--- src/components/rank/rank.vue
+++ src/components/rank/rank.vue
@@ -1,86 +1,78 @@
 <template>
-<div class="rank" ref="rank">
-  <scroll class="toplist" ref="toplist">
-    <ul>
-      <li class="item" v-for="item in topList" @click="selectItem(item)">
+  <div class="rank" ref="rank">
+    <scroll class="toplist" ref="toplist">
+      <div class="item" v-for="item in topList" @click="selectItem(item)">
         <div class="icon">
-          <img width="100" height="100" v-lazy="item.picUrl" />
+          <img width="100" height="100" v-lazy="item.picUrl"/>
         </div>
-        <ul class="songlist">
-          <li class="song" v-for="(song,index) in item.songList">
-            <span>{{`${index+1} ${song.songname} - ${song.singername}`}}</span>
-            <span></span>
-          </li>
-        </ul>
-      </li>
-    </ul>
-    <div class="loading-container" v-if="!topList.length">
-      <loading></loading>
-    </div>
-  </scroll>
-  <router-view></router-view>
-</div>
+      </div>
+      <div class="loading-container" v-if="!topList.length">
+        <loading></loading>
+      </div>
+    </scroll>
+    <router-view></router-view>
+  </div>
 </template>
 <script>
-import Scroll from 'base/scroll/scroll'
-import Loading from 'base/loading/loading'
-import {
-  playlistMixin
-} from 'common/js/mixin'
-import {
-  ERR_OK
-} from 'api/config'
+  import Scroll from 'base/scroll/scroll'
+  import Loading from 'base/loading/loading'
+  import {
+    playlistMixin
+  } from 'common/js/mixin'
+  import {
+    ERR_OK
+  } from 'api/config'
 
-import {
-  mapMutations
-} from 'vuex'
-import {
-  getTopList
-} from 'api/rank'
+  import {
+    mapMutations
+  } from 'vuex'
+  import {
+    getTopList
+  } from 'api/rank'
 
-export default {
-  mixins: [playlistMixin],
-  components: {
-    Scroll,
-    Loading,
-  },
-  data() {
-    return {
-      topList: []
+  export default {
+    mixins: [playlistMixin],
+    components: {
+      Scroll,
+      Loading,
+    },
+    data() {
+      return {
+        topList: []
+      }
+    },
+    created() {
+      this._getTopList()
+    },
+    methods: {
+      selectItem(item) {
+        this.$router.push({
+          path: `/rank/${item.id}`,
+        })
+        this.setTopList(item)
+      },
+      _getTopList() {
+        getTopList().then((res) => {
+
+          if (res.code === ERR_OK) {
+            this.topList = res.data.topList
+
+          }
+        })
+
+      },
+      handlePlaylist(playlist) {
+        const bottom = playlist.length ? '60px' : ''
+        this.$refs.rank.style.bottom = bottom
+        //dom组件变化了，手动调用刷新scroll组件
+        this.$refs.toplist.refresh()
+      },
+      ...mapMutations({
+        setTopList: 'SET_TOP_LIST'
+      })
     }
-  },
-  created() {
-    this._getTopList()
-  },
-  methods: {
-    selectItem(item) {
-      this.$router.push({
-        path: `/rank/${item.id}`,
-      })
-      this.setTopList(item)
-    },
-    _getTopList() {
-      getTopList().then((res) => {
 
-        if (res.code === ERR_OK) {
-          this.topList = res.data.topList
-
-        }
-      })
-
-    },
-    handlePlaylist(playlist) {
-      const bottom = playlist.length ? '60px' : ''
-      this.$refs.rank.style.bottom = bottom
-      //dom组件变化了，手动调用刷新scroll组件
-      this.$refs.toplist.refresh()
-    },
-    ...mapMutations({
-      setTopList: 'SET_TOP_LIST'
-    })
   }
-
-}
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
@@ -93,33 +85,19 @@ export default {
     top: 88px
     bottom: 0
     .toplist
+      background-color: $color-highlight-background
       height: 100%
       overflow: hidden
-      .item
-        display: flex
-        margin: 0 20px
-        padding-top: 20px
-        height: 100px
-        &:last-child
-          padding-bottom: 20px
-        .icon
-          flex: 0 0 100px
-          width: 100px
-          height: 100px
-        .songlist
-          flex: 1
-          display: flex
-          flex-direction: column
-          justify-content: center
-          padding: 0 20px
-          height: 100px
-          overflow: hidden
-          background: $color-highlight-background
-          color: $color-text-d
-          font-size: $font-size-small
-          .song
-            no-wrap()
-            line-height: 26px
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-around;
+      margin-top:20px
+      :nth-child(3n+1)
+        .icon img
+          width:200px
+          height:200px
+      .item .icon img
+        border-radius:50%
       .loading-container
         position: absolute
         width: 100%
